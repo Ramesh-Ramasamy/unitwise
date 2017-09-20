f45680efc2f8efdd1636a4fe2dababf8a36f54d6
Ramesh <ramesh.r@infibeam.com>
2017-09-20 13:26:56 +0530

    Migration name change

--- generators/unitwise_migration/templates/migration.rb
+++ generators/unitwise_migration/templates/migration.rb
@@ -1,14 +1,6 @@
-class ActsAsTaggableOnMigration < ActiveRecord::Migration
+class UnitwiseMigration < ActiveRecord::Migration
   def self.up
     create_table :unitwise do |t|
-      # t.column :tag_id, :integer
-      # t.column :taggable_id, :integer
-      # t.column :tagger_id, :integer
-      # t.column :tagger_type, :string
-      
-      # # You should make sure that the column created is
-      # # long enough to store the required class names.
-      # t.column :taggable_type, :string
       t.column :context, :string
       
       t.column :created_at, :datetime
