5bed3e2dda3fbc807c722a645bf380cc7d73d4dd
Ramesh <ramesh.r@infibeam.com>
2017-12-22 13:32:07 +0530

    adding utf8

--- generators/unitwise_migration/templates/migration.rb
+++ generators/unitwise_migration/templates/migration.rb
@@ -1,6 +1,6 @@
 class UnitwiseMigration < ActiveRecord::Migration
   def self.up
-    create_table (:measurement_base_units, :id => false) do |t|
+    create_table (:measurement_base_units, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8') do |t|
       t.column :names, :string
       t.column :symbol, :string
       t.column :primary_code, :string
@@ -8,7 +8,7 @@ class UnitwiseMigration < ActiveRecord::Migration
       t.column :property, :string
       t.column :dim, :string
     end
-    create_table (:measurement_derived_units, :id => false) do |t|
+    create_table (:measurement_derived_units, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8') do |t|
       t.column :names, :text
       t.column :symbol, :string
       t.column :primary_code, :string
@@ -19,9 +19,10 @@ class UnitwiseMigration < ActiveRecord::Migration
       t.column :metric, :boolean
       t.column :special, :boolean
       t.column :arbitrary, :boolean
+      t.column :priority, :integer, :null => false, :default => 0
 
     end
-    create_table (:measurement_prefix_units, :id => false) do |t|
+    create_table (:measurement_prefix_units, :options => 'ENGINE=InnoDB DEFAULT CHARSET=utf8') do |t|
       t.column :names, :string
       t.column :symbol, :string
       t.column :primary_code, :string
